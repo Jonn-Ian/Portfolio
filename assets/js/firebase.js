270ab61c5610a0b6e059552cee19b44aac1bd462
Jonn-Ian <jonniansabar@gmail.com>
2025-11-15 14:52:04 +0800

    add a data visualization portfolio

---
+++ assets/js/firebase.js
@@ -0,0 +1,24 @@
+// assets/js/firebase.js
+import { initializeApp, getApps, getApp } 
+  from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+import { getAnalytics } 
+  from "https://www.gstatic.com/firebasejs/12.6.0/firebase-analytics.js";
+import { getFirestore } 
+  from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCT3IQAcHzsK9KEqP7-COUHSRDH82P6xpI",
+  authDomain: "live-chart-counter.firebaseapp.com",
+  projectId: "live-chart-counter",
+  storageBucket: "live-chart-counter.appspot.com", // ✅ corrected
+  messagingSenderId: "123665232024",
+  appId: "1:123665232024:web:050ed4ce56ba52d71020ed",
+  measurementId: "G-51D88CBNXP"
+};
+
+// Initialize only once
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+export { app, analytics, db };
